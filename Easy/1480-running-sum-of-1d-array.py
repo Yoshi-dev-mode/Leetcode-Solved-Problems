05c58b7913e25d2319303561671aacb5d2c59911
Yoshi-dev-mode <evanyoshi19@gmail.com>
2025-11-03 08:48:54 +0800

    Add files via upload

--- Easy/1480-running-sum-of-1d-array.py
+++ Easy/1480-running-sum-of-1d-array.py
@@ -1,1 +1,11 @@
+# Problem: Running Sum of 1d Array
+# Description: Return the running sum of an array of integers.
+# My Solution: O(n) in-place update.
 
+class Solution:
+    def runningSum(self, nums):
+        container = 0
+        for i in range(len(nums)):
+            container += nums[i]
+            nums[i] = container
+        return nums
